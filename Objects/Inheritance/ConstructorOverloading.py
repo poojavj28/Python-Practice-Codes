675330222dad5077a0ad1393c17579670c68cb2e
poojavj28 <poojavajramatti28@gmail.com>
2025-03-26 16:33:13 +0530

    Initial commit

--- Objects/Inheritance/ConstructorOverloading.py
+++ Objects/Inheritance/ConstructorOverloading.py
@@ -4,7 +4,7 @@ class Demo1:
     def __init__(self):
         self.x = 200
 d3 = Demo1()
-print(d3.x)
+print(d3.x) #200
 '''
 When we create multiple constructors in same class then only 
 last declared constructor we will be invoked at the time of object creation.
